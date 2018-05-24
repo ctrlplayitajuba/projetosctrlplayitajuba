e823b6f377a1a2d8246645715da0caf98a2b16af
LAPTOP-JOH2LUUE\Ctrl Pay Aluno 203 <l.alvesctrlplay@gmail.com>
2018-05-24 10:23:55 -0300

    [ADD] Método "Move" para o movimento do carrinho para frente

---
+++ Apollo 11/Protótipo/Apollo11prototype/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour {
+
+	[SerializeField] private float speed = 1.0f;
+
+	public void Move (float time){
+		iTween.MoveBy(gameObject, iTween.Hash(
+										"x"   , time*speed, 
+									  	"time", time,
+										"easetype", iTween.EaseType.linear));
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				Move (1);
+	}
+}
