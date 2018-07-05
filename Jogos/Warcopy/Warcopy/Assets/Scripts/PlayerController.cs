925f2b452cef8fd961b0805307b8a37bc9731c28
LAPTOP-U297CEC9\Ctrl Play Aluno 204 <l.alvesctrlplay@gmail.com>
2018-07-05 11:24:55 -0300

    [ADD] movimento com joystick e NetworkManager

---
+++ Jogos/Warcopy/Warcopy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Joystick joystick;
+	private Rigidbody player;
+	private Transform mainCamera;
+	private Vector3 cameraOffset;
+	public float deadZoneLimit = 5f;
+	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		cameraOffset = new Vector3 (0f, 35f, -25f);
+		joystick = FindObjectOfType<Joystick> ();
+		player = GetComponent<Rigidbody>();
+		mainCamera = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player.velocity = new Vector3 (joystick.Horizontal * speed, player.velocity.y, joystick.Vertical * speed);
+		MoveCamera ();
+	}
+
+	/// <summary>
+	/// Controle do movimento da câmera
+	/// </summary>
+	void MoveCamera() {
+		Vector3 deadZone = this.transform.position - mainCamera.position + cameraOffset;
+		if (deadZone.magnitude > deadZoneLimit) {
+			mainCamera.Translate (deadZone.normalized * speed * Time.deltaTime, Space.World);
+		}
+	}
+}
