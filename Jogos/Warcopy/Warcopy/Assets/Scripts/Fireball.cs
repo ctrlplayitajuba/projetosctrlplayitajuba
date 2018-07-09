86ed5e62da25d1d18a9cac57492af1f746f6d2ea
LAPTOP-312G1F75\Ctrl Play Aluno 12 <l.alvesctrlplay@gmail.com>
2018-07-09 15:35:17 -0300

    [TODO] Criar objeto server-side para a explosão da bola de fogo

--- Jogos/Warcopy/Warcopy/Assets/Scripts/Fireball.cs
+++ Jogos/Warcopy/Warcopy/Assets/Scripts/Fireball.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Fireball : MonoBehaviour {
+public class Fireball : NetworkBehaviour {
 	[SerializeField] private float force = 500f;
 	void OnCollisionEnter(Collision collision){
-		Vector3 direction = collision.transform.position - this.transform.position;
+		CmdPush (collision.gameObject);
+	}
+
+	[Command]
+	void CmdPush (GameObject ob){
 		Destroy (gameObject);
-		direction = direction.normalized;
-		collision.rigidbody.AddExplosionForce (force, this.transform.position, 1f);
+		ob.GetComponent<Rigidbody>().AddExplosionForce (force, this.transform.position, 1f);
 	}
 }
